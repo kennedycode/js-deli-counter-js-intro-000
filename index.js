3d7893a1acae159f1db7d2b9c55d729856374711
Andrew Kennedy <akennedycode@gmail.com>
2020-07-30 13:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
     return "The line is currently empty."
   }
 
-  const numbersAndNames = []
+  var numbersAndNames = []
 
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
