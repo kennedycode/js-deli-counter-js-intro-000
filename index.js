f635a0a9112850fbb6a55955189647a7f2324507
Andrew Kennedy <akennedycode@gmail.com>
2020-07-30 14:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line) {
   var katzDeliLine = [];
 
   for (let i = 0, l = line.length; i < l; i++) {
-    katzDeliLine.push(`${i + 1}. ${line[i]}` )
+    katzDeliLine.push(`${i + 1}. ${line[i]}`)
   }
 
   return `The line is currently: ${katzDeliLine}`
